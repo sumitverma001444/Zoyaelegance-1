e68a2138a2d8f040eb60fb11970b0655ddc737c1
zoyaelegance <babahacket4@gmail.com>
2026-01-03 21:04:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin: process.env.FRONTEND_URL || ["http://localhost:5173", "http://localhost:5174"],
+ origin: process.env.FRONTEND_URL || ["https://zoyaelegance.onrender.com", "http://localhost:5174"],
  credentials:true
 }))
 
