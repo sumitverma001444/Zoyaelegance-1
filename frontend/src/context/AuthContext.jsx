b45bb0172b0fb65936eb4f251c8e6128d1e8867c
zoyaelegance <babahacket4@gmail.com>
2026-01-06 15:49:44 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://frontend-kflh.onrender.com"
+    let serverUrl = "zoyaelegance.com"
 
     let value = {
        serverUrl
